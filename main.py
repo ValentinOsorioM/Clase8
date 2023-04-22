75632f08c777605dae097cb73307438c1acf7f61
Valentin <vaosoriom06@gmail.com>
2023-04-22 15:46:44 -0500

    Crea varias listas y muestra su tamaño, crear el .gitignore

---
+++ main.py
@@ -0,0 +1,57 @@
+
+'''
+Hasta ahora hemos
+trabajado conn variables
+que permiten alamcenar
+un unico valor
+'''
+
+
+edad = 12
+
+altura = 1.80
+
+nombre = "Valentin"
+
+estado = True
+
+'''
+En python podemos 
+usar una variable
+que alamcena una
+colección de datos
+y luego accederla 
+usando un subindice
+'''
+
+# Lista de enteros
+lista1 = [10, 5, 3, 9]
+# Lista de decimales
+lista2 = [1.78, 2.66, 1.55, 89.4, 6.9]
+# Lista de string
+lista3 = ["Lunes","Martes","Miércoles"]
+'''
+lista de elementos 
+de distinto tipo
+'''
+
+lista4 = ["juan",45,1,92,False]
+
+
+if __name__ == '__main__':
+
+    '''
+    Cantidad de elementos de cada lista:
+    '''
+    print(len(lista1))
+    print(len(lista2))
+    print(len(lista3))
+    print(len(lista4))
+
+    print()
+    print(lista1)
+    lista1[0] = 1
+    print()
+    print(lista1)
+
+    print(lista1[3])
